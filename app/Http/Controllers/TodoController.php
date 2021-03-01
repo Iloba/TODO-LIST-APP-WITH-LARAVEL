62494bd6dd4d254ab6f0f1ec9d4a705ebaefd4b6
Iloba <scarlettsney@gmail.com>
2021-03-01 14:28:14 +0100

    day3

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 //Import Todo Model
 use App\Models\Todo;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TodoController extends Controller
 {
@@ -13,7 +15,11 @@ class TodoController extends Controller
 
     //Show all Todos
     public function index(){
-        return view('todos.index');
+
+        //Get All Todos
+        $todos = Todo::all();
+
+        return view('todos.index')->with(['todos' => $todos]);
     }
 
     public function create(){
@@ -28,14 +34,32 @@ class TodoController extends Controller
     //Store Todos
     public function store(Request $request){
         
-        //Validate Form request
-        $request->validate([
+        //Validate Form request (Rules)
+        $rules = [
             'title' => 'required|max:255'
-        ]);
+        ];
 
-        //Store Todo
+        //Customized Messages
+        $messages = [
+            'title.max' => 'Todo Title Should not be greater than 255 Characters',
+            'title.required' => 'You Must Add a Todo TItle'
+        ];
+
+       //Using the Validator Facade will help Us to Customize our error messages
+       $validator = Validator::make($request->all(), $rules, $messages);
+
+        //If Validator Fails redirect Back with Error
+        if($validator->fails()){
+            return redirect()->back()
+            ->withErrors($validator)
+            ->withInput();
+        }
+
+        //Store Todo if Validation is Successful
         Todo::create($request->all());
 
+        //Return Redirect with Success Message
         return redirect()->back()->with('status', 'Todo Creation Successful');
+        
     }
 }
